cd419fef2fd52d40755c7f188eab76cd1d31a82d
indramaulin76 <indramaulin@gmail.com>
2025-12-21 15:40:39 +0700

    Fix mixed content error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,21 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- 1. Tambah baris ini
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        // 2. Tambah blok ini
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
